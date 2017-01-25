fd9313437c28cea4621bb3c2895b1cfeac29a9a2
alexandarp <alexandar.podobnik@gmail.com>
2017-01-25 15:39:45 -0500

    IO-860 adding descriptions in variables

--- variables.tf
+++ variables.tf
@@ -1,34 +1,44 @@
-variable "cluster_id" {}
+variable "cluster_id" {
+  description = "Cluster ID you want to use"
+}
 
 variable "aws_region" {
+  description = "Region you want to use"
   default = "ca-central-1"
 }
 
 variable "availability_zone" {
+  description = "Availability zone you want to use"
   default = "ca-central-1a"
 }
 
 variable "port" {
+  description = "Port number you want to use"
   default = 6379
 }
 
 variable "engine_version" {
+  description = "Redis engine version you want to use"
   default = "2.8.24"
 }
 variable "node_type" {
+  description = "Node type you want to use"
   default = "cache.m4.large"
 }
 
 variable "num_cache_nodes" {
+  description = "The number of cache nodes you want"
   default = 1
 }
 
 variable "security_group_ids" {
-  default = ""
+  description = "The security_group ids to attach the instance to"
+  #default = "sg-1fec2e76"
 }
 
 variable "subnet_group_name" {
-  default = ""
+  description = "The subnet to launch the instance into"
+  #default = "openshift-sandbox"
 }
 
 variable "parameter_group_name" {

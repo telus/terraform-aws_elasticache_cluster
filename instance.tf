c1e666879d0b07bb342245d59e620f2456d3a678
alexandarp <alexandar.podobnik@gmail.com>
2017-01-25 15:38:32 -0500

    IO-860 fixing syntax error for sec group

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "default" {
   availability_zone = "${var.availability_zone}"
   port = "${var.port}"
   num_cache_nodes = "${var.num_cache_nodes}"
-  security_group_ids "${var.security_group_ids}"
+  security_group_ids = ["${var.security_group_ids}"]
   subnet_group_name = "${var.subnet_group_name}"
   parameter_group_name = "${var.parameter_group_name}"
 }

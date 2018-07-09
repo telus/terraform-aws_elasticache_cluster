22f17658a34b0bc20084879d82b898858eb426e3
Alexandar Podobnik <alexandar.podobnik@gmail.com>
2018-07-09 15:31:26 -0400

    Commenting output for name
    name output is broken

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "cache_nodes" {
   value = "${aws_elasticache_cluster.default.cache_nodes}"
 }
 
-output "instance_name" {
-  value = "${aws_elasticache_cluster.default.name}"
-}
+# output "instance_name" {
+#   value = "${aws_elasticache_cluster.default.name}"
+# }
